5f118d3add60e2a0365fa3bbc976693c3513bd31
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-02-04 22:25:19 -0300

    Alteração das requisições -> Utilizando a dependência DIO

--- spotted/lib/app/repository/http_service.dart
+++ spotted/lib/app/repository/http_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart';
 import '../view/food_page/post_model.dart';
 
@@ -7,27 +8,22 @@ class FoodRepository {
   final String postsURL = "https://jsonplaceholder.typicode.com/posts";
 
   Future<List<Post>> getPosts() async {
+    final response = await Dio().get(postsURL);
 
-    Response res = await get(
-      Uri.parse(postsURL),
-      headers: {"Accept": "application/json"}
-    );
-
-    if (res.statusCode == 200) {
-
-      //Se o servidor retornar um response OK, vamos fazer o parse no JSON
-      List<dynamic> body = jsonDecode(res.body);
+    if (response.statusCode == 200) {
+      // se o servidor retornar um response OK, vamos fazer o parse no JSON
+      List<dynamic> body = response.data;
 
       List<Post> posts = body
-        .map(
-          (dynamic item) => Post.fromJson(item),
-        )
-        .toList();
+          .map(
+            (dynamic item) => Post.fromJson(item),
+          )
+          .toList();
 
       return posts;
     } else {
-      //Se a resposta não for OK , lançamos um erro
-      throw "Não foi possível recuperar as comidas.";
+      // se a responsta não for OK , lançamos um erro
+      throw "Não foi possível recuperar os posts.";
     }
   }
 }
