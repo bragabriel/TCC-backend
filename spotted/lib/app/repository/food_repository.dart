2571b0e32466114eb9ecb666e6cd3fbfd87c9453
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-02-04 23:19:13 -0300

    feat: Tela de Comida & Consumo de API

--- spotted/lib/app/repository/food_repository.dart
+++ spotted/lib/app/repository/food_repository.dart
@@ -1,29 +1,30 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:http/http.dart';
+import 'package:spotted/app/model/food_model.dart';
 import '../view/food_page/post_model.dart';
 
 class FoodRepository {
   
-  final String postsURL = "https://jsonplaceholder.typicode.com/posts";
+  final String postsURL = "http://localhost:8080/api/comida?paginaAtual=0&qtdPorPagina=10";
 
-  Future<List<Post>> getPosts() async {
+  Future<List<Food>> getFood() async {
     final response = await Dio().get(postsURL);
 
     if (response.statusCode == 200) {
       // se o servidor retornar um response OK, vamos fazer o parse no JSON
       List<dynamic> body = response.data;
 
-      List<Post> posts = body
+      List<Food> foodList = body
           .map(
-            (dynamic item) => Post.fromJson(item),
+            (dynamic item) => Food.fromJson(item),
           )
           .toList();
 
-      return posts;
+      return foodList;
     } else {
       // se a responsta não for OK , lançamos um erro
-      throw "Não foi possível recuperar os posts.";
+      throw "Não foi possível recuperar os dados.";
     }
   }
 }
